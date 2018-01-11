efd87b4fbe790efe587c055e4ceb2d66b430726a
Robert <robertdibejar@gmail.com>
2018-01-11 18:07:10 -0300

    se crea una estructura de carpetas para manejar redu y se crean las rutas de home, login y register

---
+++ src/_container/Register.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+
+  render(){
+    return (
+      <h1>Resgristación</h1>
+    );
+  }
+}
+
+export default Register;
--- src/index.js
+++ src/index.js
@@ -4,176 +4,12 @@ import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
 import { getAbsoluteFilePath } from "./util";
 import Inicio from "./inicio";
-import Final from "./final";
+import Login from "./_container/Login";
+import Register from "./_container/Register";
 
-import Ex1 from "./fundamentos/01";
-import Ex2 from "./fundamentos/02";
-import Ex3 from "./fundamentos/03";
-import { ejercicio4A, ejercicio4B } from "./fundamentos/04";
-import Ex5 from "./fundamentos/05";
-import Ex6 from "./fundamentos/06";
-import Ex7 from "./fundamentos/07";
-import Ex8 from "./fundamentos/08";
-import Ex9 from "./fundamentos/09";
-
-import Ex1Sol from "./fundamentos/soluciones/01";
-import Ex2Sol from "./fundamentos/soluciones/02";
-import Ex3Sol from "./fundamentos/soluciones/03";
-import { ejercicio4A as ejercicio4ASol, ejercicio4B as ejercicio4BSol } from "./fundamentos/soluciones/04";
-import Ex5Sol from "./fundamentos/soluciones/05";
-import Ex6Sol from "./fundamentos/soluciones/06";
-import Ex7Sol from "./fundamentos/soluciones/07";
-import Ex8Sol from "./fundamentos/soluciones/08";
-import Ex9Sol from "./fundamentos/soluciones/09";
 // import registerServiceWorker from "./registerServiceWorker";
 import "normalize.css";
 import "./index.css";
-
-function exWithDOMElement(ex) {
-  return class ExWrapper extends React.Component {
-    componentDidMount() {
-      ex(this.el);
-    }
-
-    shouldComponentUpdate() {
-      return false;
-    }
-
-    render() {
-      return (
-        <div
-          ref={el => {
-            this.el = el;
-          }}
-        />
-      );
-    }
-  };
-}
-
-function exNumber(index) {
-  const num = index + 1;
-  return num < 10 ? "0" + num : String(num);
-}
-
-function ExerciseLink({ to, children }) {
-  return (
-    <a
-      style={{ cursor: "pointer" }}
-      onClick={ev => {
-        ev.preventDefault();
-        const href =
-          "/__open-stack-frame-in-editor?fileName=" +
-          window.encodeURIComponent(getAbsoluteFilePath(to));
-        fetch(href);
-      }}
-    >
-      {children}
-    </a>
-  );
-}
-
-function Foundation({ match }) {
-  const exercises = [
-    [exWithDOMElement(Ex1), exWithDOMElement(Ex1Sol)],
-    [Ex2, Ex2Sol],
-    [Ex3, Ex3Sol],
-    [[exWithDOMElement(ejercicio4A), ejercicio4B], [exWithDOMElement(ejercicio4ASol), ejercicio4BSol]],
-    [Ex5, Ex5Sol],
-    [Ex6, Ex6Sol],
-    [Ex7, Ex7Sol],
-    [Ex8, Ex8Sol],
-    [Ex9, Ex9Sol]
-  ];
-
-  return (
-    <Switch>
-      {exercises.map(([Ex, Sol], index) => [
-        <Route
-          key={0}
-          path={`/fundamentos/${index + 1}`}
-          exact
-          render={props =>
-            <div>
-              <div
-                style={{
-                  background: "#e0e0e0",
-                  padding: 10,
-                  fontSize: ".8em",
-                  marginBottom: 5,
-                  fontStyle: "italic"
-                }}
-              >
-                El archivo que corresponde a este ejercicio es{" "}
-                <ExerciseLink to={`/src/fundamentos/${exNumber(index)}.js`}>
-                  <b>
-                    /src/fundamentos/{exNumber(index)}.js
-                  </b>
-                </ExerciseLink>. Abrilo en tu editor de texto.
-              </div>
-              <div style={{ padding: 10 }}>
-                {Array.isArray(Ex)
-                  ? Ex.map((Ex, index) => <Ex key={index} {...props} />)
-                  : <Ex {...props} />}
-              </div>
-            </div>}
-        />,
-        <Route
-          key={1}
-          path={`/fundamentos/${index + 1}/solucion`}
-          render={props =>
-            <div>
-              <div
-                style={{
-                  background: "#e0e0e0",
-                  padding: 10,
-                  fontSize: ".8em",
-                  marginBottom: 5,
-                  fontStyle: "italic"
-                }}
-              >
-                El archivo que corresponde a esta solución del ejercicio es{" "}
-                <ExerciseLink to={`/src/fundamentos/soluciones/${exNumber(index)}.js`}>
-                  <b>
-                    /src/fundamentos/soluciones/{exNumber(index)}.js
-                  </b>
-                </ExerciseLink>. Abrilo en tu editor de texto.
-              </div>
-              <div style={{ padding: 10 }}>
-                {Array.isArray(Sol)
-                  ? Sol.map((Sol, index) => <Sol key={index} {...props} />)
-                  : <Sol {...props} />}
-              </div>
-            </div>}
-        />
-      ])}
-      <Route
-        render={() =>
-          <div className="root">
-            <h1>Workshop: Fundamentos</h1>
-            <ul className="rootList">
-              {exercises.map((ex, index) =>
-                <li key={index}>
-                  <Link to={`/fundamentos/${index + 1}`}>
-                    Ejercicio {index + 1}
-                  </Link>{" "}
-                  <Link to={`/fundamentos/${index + 1}/solucion`}>
-                    <span
-                      role="img"
-                      aria-label="solución"
-                      style={{ fontSize: ".6em", fontStyle: "italic", color: '#a0a0a0' }}
-                    >
-                      (👀 solución)
-                    </span>
-                  </Link>
-                </li>
-              )}
-            </ul>
-          </div>}
-      />
-    </Switch>
-  );
-}
 
 function Root() {
   return (
@@ -182,23 +18,29 @@ function Root() {
       <ul className="rootList">
 
         <li>
-          <Link to="/inicio">Versión inicial (estática) de la aplicación</Link>
+          <Link to="/home">Versión inicial (estática) de la aplicación</Link>
         </li>
-
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/Register">Register</Link>
+        </li>
       </ul>
     </div>
   );
 }
 
 ReactDOM.render(
-  process.env.NODE_ENV === "production"
-    ? <Final />
-    : <BrowserRouter>
-        <Switch>
-          <Route path="/inicio" component={Inicio} />
-          <Route component={Root} />
-        </Switch>
-      </BrowserRouter>,
+
+  <BrowserRouter>
+    <Switch>
+      <Route path="/home" component={Inicio} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route component={Root} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
